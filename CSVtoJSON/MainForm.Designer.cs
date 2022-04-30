c5853cc7effe40a85ed153c4d659dc20768cdd49
adriana634 <50183872+adriana634@users.noreply.github.com>
2022-04-30 21:57:11 +0200

    CSV to JSON

--- CSVtoJSON/MainForm.Designer.cs
+++ CSVtoJSON/MainForm.Designer.cs
@@ -28,19 +28,88 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.convertCsvToJsonButton = new System.Windows.Forms.Button();
+            this.convertJsonToCsvButton = new System.Windows.Forms.Button();
+            this.resultRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.contextMenuStrip = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.copyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.Filter = "CSV files|*.csv";
+            // 
+            // convertCsvToJsonButton
+            // 
+            this.convertCsvToJsonButton.Location = new System.Drawing.Point(37, 41);
+            this.convertCsvToJsonButton.Name = "convertCsvToJsonButton";
+            this.convertCsvToJsonButton.Size = new System.Drawing.Size(622, 66);
+            this.convertCsvToJsonButton.TabIndex = 0;
+            this.convertCsvToJsonButton.Text = "Convert CSV to JSON";
+            this.convertCsvToJsonButton.UseVisualStyleBackColor = true;
+            this.convertCsvToJsonButton.Click += new System.EventHandler(this.convertCsvToJsonButton_Click);
+            // 
+            // convertJsonToCsvButton
+            // 
+            this.convertJsonToCsvButton.Location = new System.Drawing.Point(37, 113);
+            this.convertJsonToCsvButton.Name = "convertJsonToCsvButton";
+            this.convertJsonToCsvButton.Size = new System.Drawing.Size(622, 66);
+            this.convertJsonToCsvButton.TabIndex = 1;
+            this.convertJsonToCsvButton.Text = "Convert JSON to CSV";
+            this.convertJsonToCsvButton.UseVisualStyleBackColor = true;
+            this.convertJsonToCsvButton.Click += new System.EventHandler(this.convertJsonToCsvButton_Click);
+            // 
+            // resultRichTextBox
+            // 
+            this.resultRichTextBox.ContextMenuStrip = this.contextMenuStrip;
+            this.resultRichTextBox.Location = new System.Drawing.Point(37, 212);
+            this.resultRichTextBox.Name = "resultRichTextBox";
+            this.resultRichTextBox.ReadOnly = true;
+            this.resultRichTextBox.Size = new System.Drawing.Size(622, 274);
+            this.resultRichTextBox.TabIndex = 2;
+            this.resultRichTextBox.Text = "";
+            // 
+            // contextMenuStrip
+            // 
+            this.contextMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.copyToolStripMenuItem});
+            this.contextMenuStrip.Name = "contextMenuStrip";
+            this.contextMenuStrip.Size = new System.Drawing.Size(135, 26);
+            // 
+            // copyToolStripMenuItem
+            // 
+            this.copyToolStripMenuItem.Name = "copyToolStripMenuItem";
+            this.copyToolStripMenuItem.Size = new System.Drawing.Size(134, 22);
+            this.copyToolStripMenuItem.Text = "Copy result";
+            this.copyToolStripMenuItem.Click += new System.EventHandler(this.copyToolStripMenuItem_Click);
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(702, 498);
+            this.Controls.Add(this.resultRichTextBox);
+            this.Controls.Add(this.convertJsonToCsvButton);
+            this.Controls.Add(this.convertCsvToJsonButton);
             this.Name = "MainForm";
             this.Text = "CSV/JSON converter";
+            this.contextMenuStrip.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private OpenFileDialog openFileDialog;
+        private SaveFileDialog saveFileDialog;
+        private Button convertCsvToJsonButton;
+        private Button convertJsonToCsvButton;
+        private RichTextBox resultRichTextBox;
+        private ContextMenuStrip contextMenuStrip;
+        private ToolStripMenuItem copyToolStripMenuItem;
     }
 }
--- CSVtoJSON/MainForm.cs
+++ CSVtoJSON/MainForm.cs
@@ -1,3 +1,7 @@
+using CsvHelper;
+using System.Globalization;
+using System.Text.Json;
+
 namespace CSVtoJSON
 {
     public partial class MainForm : Form
@@ -5,6 +9,41 @@ namespace CSVtoJSON
         public MainForm()
         {
             InitializeComponent();
+        }
+
+        private void convertCsvToJsonButton_Click(object sender, EventArgs e)
+        {
+            DialogResult dialog_result = this.openFileDialog.ShowDialog();
+
+            if (dialog_result == DialogResult.OK)
+            {
+                string filename = this.openFileDialog.FileName;
+
+                using StreamReader reader = new StreamReader(filename);
+                using CsvReader csv = new CsvReader(reader, CultureInfo.InvariantCulture);
+
+                IEnumerable<dynamic> records = csv.GetRecords<dynamic>();
+
+                JsonSerializerOptions options = new JsonSerializerOptions { WriteIndented = true };
+                string json = JsonSerializer.Serialize(records, options);
+
+                this.resultRichTextBox.Text = json;
+            }
+        }
+
+        private void convertJsonToCsvButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void copyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string content = this.resultRichTextBox.Text;
+            
+            if (string.IsNullOrEmpty(content) == false)
+            {
+                Clipboard.SetText(content);
+            }
         }
     }
 }
